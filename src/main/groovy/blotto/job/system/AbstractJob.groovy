2a941203a86a0f7bbb986f950a0ad370924df59e
xaos <xaos@z-inc.net>
2015-02-13 18:03:47 +0500

    search next run

--- src/main/groovy/blotto/job/system/AbstractJob.groovy
+++ src/main/groovy/blotto/job/system/AbstractJob.groovy
@@ -1,6 +1,10 @@
 package blotto.job.system
 
 import groovy.util.logging.Log4j
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.scheduling.support.CronSequenceGenerator
+
+import java.lang.reflect.Method
 
 @Log4j
 public class AbstractJob {
@@ -25,6 +29,9 @@ public class AbstractJob {
     public void run(Closure job) {
         try {
             synchronized (lock) {
+                while (lock.inProgress) {
+                    Thread.sleep(1000)
+                }
                 lock.inProgress = true
             }
             lastStart = new Date()
@@ -39,5 +46,32 @@ public class AbstractJob {
                 lock.inProgress = false
             }
         }
+    }
+
+    Date getNextRun(){
+        // TODO find closest with CronSequenceGenerator
+        new CronSequenceGenerator("").next(new Date())
+        return new Date()
+    }
+
+    private List<Method> getMethodsAnnotatedWithScheduled() {
+        final List<Method> methods = new ArrayList<Method>();
+        Class<?> klass = this.class;
+        while (klass != Object.class) {
+            // need to iterated thought hierarchy in order to retrieve methods from above the current instance
+            // iterate though the list of methods declared in the class represented by klass variable, and add those annotated with the specified annotation
+            final List<Method> allMethods = new ArrayList<Method>(Arrays.asList(klass.getDeclaredMethods()));
+            for (final Method method : allMethods) {
+                if (method.isAnnotationPresent(Scheduled.class)) {
+                    Scheduled annotInstance = method.getAnnotation(Scheduled.class);
+                    if (annotInstance.cron()) {
+                        methods.add(method);
+                    }
+                }
+            }
+            // move to the upper class in the hierarchy in search for more methods
+            klass = klass.getSuperclass();
+        }
+        return methods;
     }
 }
