33498bd843312c1fe1723c8dcce2432cf3e6dfda
xaos <xaos@z-inc.net>
2015-03-02 11:01:36 +0500

    sql fixes

--- src/main/resources/liquibase/sql/r0.01.01_update.sql
+++ src/main/resources/liquibase/sql/r0.01.01_update.sql
@@ -11,7 +11,8 @@
 UPDATE DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = 'home-fca85f8b86 (192.168.0.100)', LOCKGRANTED = '2015-02-23 01:57:24.156' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Changeset G:\dev\colonel-blotto-online\src\main\resources\liquibase\updates\r0.01.01.xml::r0.01.01-1::xaos
-ALTER TABLE player ALTER COLUMN strategy_updated RENAME TO strategy_last_updated;
+--ALTER TABLE player ALTER COLUMN strategy_updated RENAME TO strategy_last_updated;
+ALTER TABLE player CHANGE COLUMN strategy_updated strategy_last_updated timestamp;
 
 INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('r0.01.01-1', 'xaos', 'G:\dev\colonel-blotto-online\src\main\resources\liquibase\updates\r0.01.01.xml', NOW(), 5, '7:cef338eb3ce2a8b08c286b3fd16f5ada', 'renameColumn', '', 'EXECUTED', '3.3.2');
 
--- src/main/resources/liquibase/sql/r0.01.03_update.sql
+++ src/main/resources/liquibase/sql/r0.01.03_update.sql
@@ -11,7 +11,8 @@
 UPDATE DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = 'home-fca85f8b86 (192.168.0.100)', LOCKGRANTED = '2015-03-01 22:20:28.531' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Changeset G:\dev\colonel-blotto-online\src\main\resources\liquibase\updates\r0.01.03.xml::r0.01.03-1::xaos
-ALTER TABLE player DROP CONSTRAINT uk_9glopf0unn9e6uytmk75tox52;
+--ALTER TABLE player DROP CONSTRAINT uk_9glopf0unn9e6uytmk75tox52;
+ALTER TABLE player DROP INDEX uk_9glopf0unn9e6uytmk75tox52;
 
 INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('r0.01.03-1', 'xaos', 'G:\dev\colonel-blotto-online\src\main\resources\liquibase\updates\r0.01.03.xml', NOW(), 7, '7:58dca2f727019613a3d4214019f5671a', 'dropUniqueConstraint', '', 'EXECUTED', '3.3.2');
 
